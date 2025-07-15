8216aeb40aa9a51f3e70d93c73759d419c0933cd
granitespinoza <112722319+granitespinoza@users.noreply.github.com>
2025-07-15 18:34:44 -0500

    Update testbench with waveform dump and higher timeout

--- tb/arm_tb.sv
+++ tb/arm_tb.sv
@@ -24,12 +24,18 @@ module arm_tb;
     reset = 0;
   end
 
+  // Dump waves for easier debugging
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, arm_tb);
+  end
+
   // Track number of cycles and stop after a timeout
   integer cycle_count = 0;
   always @(posedge clk) begin
     cycle_count <= cycle_count + 1;
     $display("cycle %0d PC=%h", cycle_count, dut.arm_inst.dp.PC);
-    if (cycle_count > 400) begin
+    if (cycle_count > 1000) begin
       $display("Simulation result: R10 = 0x%08h", dut.arm_inst.dp.rf.rf[10]);
       if (dut.arm_inst.dp.rf.rf[10] == 32'd1) begin
         $display("Simulation succeeded");
